f4d28ec36ad05a301f003723708edc5af6b9f471
DavidZG312 <davidzg312@hotmail.com>
2023-07-06 22:28:01 -0500

    detection image and screen show with rectangle

---
+++ ScriptInfOld.py
@@ -0,0 +1,151 @@
+import keyboard
+import pyautogui as pa
+import random
+import time
+time.sleep(2)
+for i in range(3):
+    time.sleep(.2)
+    pa.click(x=874, y=546)
+    time.sleep(.2)
+    pa.click(x=969, y=832)
+    time.sleep(.2)
+    pa.click(x=963, y=634)
+    time.sleep(.2)
+    pa.click(x=920, y=782)
+    time.sleep(.2)
+    pa.click(x=971, y=632)
+
+    # time.sleep(.2)
+    # pa.click(x=874, y=546)
+    # time.sleep(.2)
+    # pa.click(x=969, y=832)
+    # time.sleep(.2)
+    # pa.click(x=963, y=634)
+    # time.sleep(.2)
+    # pa.click(x=920, y=782)
+    # time.sleep(.2)
+    # pa.click(x=938, y=660) 
+
+##TAKE BB
+""" time.sleep(5)
+for i in range(31):
+    time.sleep(.5)
+    pa.click(x=1001, y=613)
+    time.sleep(.5)
+    pa.click(x=1098, y=618)
+    time.sleep(.5)
+    pa.click(x=889, y=679) """
+
+##for i in range(21):
+##ANTORCHAS
+""" for i in range(4):
+    timesleep = 5
+    if i != 0:
+        timesleep = 150
+    time.sleep(timesleep)
+    for i in range(50):
+        time.sleep(3)
+        pa.typewrite('d')  """
+""" time.sleep(2)
+print(pa.position()) """
+
+""" for i in range(5):
+    time.sleep(2)
+    print(pa.position()) """
+
+""" time.sleep(2)
+print(pa.position()) """
+
+##INFIERNO
+""" 
+def sript_repetitivo_1():
+    time.sleep(1.5)
+    pa.typewrite('.') 
+    time.sleep(14)
+    pa.typewrite('.') 
+    time.sleep(.5)
+
+def sript_repetitivo_2(x, y):
+    time.sleep(.7)
+    pa.click(x=x, y=y) 
+
+def auto_clicker_space():
+    time.sleep(1.5)
+    pa.typewrite('.') 
+    # time.sleep(2.5)
+    # for i in range(20):
+    #     time.sleep(.3)
+    #     pa.click(x=1456, y=159) 
+    time.sleep(5)
+    pa.typewrite('.') 
+    
+for i in range(300):
+    time.sleep(5)
+    ##Open map city
+    pa.typewrite('m') 
+    pa.typewrite('z')
+    ##Click ruta
+    time.sleep(.5)
+    pa.click(x=859, y=691)
+    #Click enter to inf
+    time.sleep(4)
+    pa.typewrite('m') 
+    pa.typewrite('z') 
+    pa.click(x=896, y=560)
+    time.sleep(.5)
+    pa.click(x=964, y=704)
+
+
+    time.sleep(5)
+    pa.typewrite('m') 
+    time.sleep(.5)
+    pa.click(x=961, y=511)
+    time.sleep(1.2)
+    pa.click(x=1043, y=418)
+
+    ##SCRIPT REPETITIVO
+
+    sript_repetitivo_1()
+    pa.click(x=1277, y=418)
+
+
+    ##SCRIPT REPETITIVO
+
+    sript_repetitivo_1()
+    pa.click(x=730, y=378)
+
+    ##SCRIPT REPETITIVO
+    sript_repetitivo_1()
+    pa.click(x=644, y=691) 
+
+    ##SCRIPT REPETITIVO
+    sript_repetitivo_1()
+    pa.click(x=1193, y=649) 
+            
+##SCRIPT REPETITIVO
+    sript_repetitivo_1()
+    pa.click(x=1276, y=553) 
+            
+    ##SCRIPT REPETITIVO
+    sript_repetitivo_1()
+    time.sleep(1)
+    pa.click(x=731, y=512)
+    time.sleep(1)
+    pa.click(x=813, y=418) 
+
+    ##SCRIPT REPETITIVO
+    sript_repetitivo_1()
+    pa.click(x=960, y=376) 
+
+    ##SCRIPT REPETITIVO
+    sript_repetitivo_1()
+    pa.click(x=960, y=650) 
+
+    ##SCRIPT REPETITIVO
+    sript_repetitivo_1()
+    pa.click(x=1361, y=383) 
+    time.sleep(.5)
+    pa.typewrite('m') 
+    pa.click(x=942, y=633) 
+
+ """
