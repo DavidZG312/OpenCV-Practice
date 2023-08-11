fd3e034b95d4b3038580ab72308e76132bf24b0d
DavidZG312 <davidzg312@hotmail.com>
2023-08-10 20:23:38 -0500

    Real Time

--- real_time/vision.py
+++ real_time/vision.py
@@ -2,21 +2,27 @@ import cv2 as cv
 import numpy as np
 import os
 
+class Vision:
 
-def findClickPositions(nedle_img_path,haystack_img_path,threshold = 0.85, debug_mode = None):
-    haystack_img = cv.imread(haystack_img_path, cv.IMREAD_UNCHANGED)
-    needle_img = cv.imread(nedle_img_path, cv.IMREAD_UNCHANGED)
-    # haystack_img = cv.imread('group_rectangles/imgs/MultipleItems.PNG', cv.IMREAD_UNCHANGED)
-    # needle_img = cv.imread('group_rectangles/imgs/Item.PNG', cv.IMREAD_UNCHANGED)
+    #properties
+    needle_img = None
+    needle_w = 0
+    needle_h = 0
+    method = None
 
-    needle_w = needle_img.shape[1]
-    needle_h = needle_img.shape[0]
 
-    # result = cv.matchTemplate(haystack_img, needle_img,cv.TM_SQDIFF_NORMED) 
-    method = cv.TM_CCOEFF_NORMED
-    result = cv.matchTemplate(haystack_img, needle_img, method) 
-    print('Hellouuuuu')
-    print(result)
+    #constructor
+    def _init_(self,nedle_img_path, method = cv.TM_CCOEFF_NORMED):
+        self.needle_img = cv.imread(nedle_img_path, cv.IMREAD_UNCHANGED)
+
+        self.needle_w = self.needle_img.shape[1]
+        self.needle_h = self.needle_img.shape[0]
+
+        self.method = method
+
+def find(self, haystack_img ,threshold = 0.85, debug_mode = None):
+    
+    result = cv.matchTemplate(haystack_img, self.needle_img, self.method) 
 
     locations = np.where(result >= threshold)
     # threshold = 0.17
@@ -27,7 +33,7 @@ def findClickPositions(nedle_img_path,haystack_img_path,threshold = 0.85, debug_
     # First we need to create the list of [x, y, w, h] rectangles
     rectangles = []
     for loc in locations:
-        rect = [int(loc[0]), int(loc[1]), needle_w, needle_h]
+        rect = [int(loc[0]), int(loc[1]), self.needle_w, self.needle_h]
         rectangles.append(rect)
 
     # rectangles, weights = cv.groupRectangles(rectangles,1,0.5)
@@ -62,12 +68,9 @@ def findClickPositions(nedle_img_path,haystack_img_path,threshold = 0.85, debug_
             elif debug_mode == 'points': 
                 cv.drawMarker(haystack_img, (center_x, center_y), marker_color, marker_type)
 
-        if debug_mode:
-            cv.imshow('Matches', haystack_img)
-            cv.waitKey()
-            #cv.imwrite('result.jpg', haystack_img)
+    if debug_mode:
+        cv.imshow('Matches', haystack_img)
+        #cv.waitKey()
+        #cv.imwrite('result.jpg', haystack_img)
 
     return points
-
-points = findClickPositions('group_rectangles/imgs/Item.PNG','group_rectangles/imgs/MultipleItems.PNG',debug_mode='points')
-print(points)
